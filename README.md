5a6b152dcc3e795eb44a32f0f91eb132bdf72abc
EvilCommunist <AT2415216@yandex.ru>
2025-08-16 04:35:56 +0300

    Beginning to create NN

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Neuro simulator based on perceptrons
+
+Work logic: <br>
+![image](./source/dhpqnj5vsmliqx3nil68ybs-ozw.png)<br>
+*Source: https://habr.com/ru/articles/514372/* weights dimension is on the links between nodes (neurons).
--- kernel/neuro.h
+++ kernel/neuro.h
@@ -5,8 +5,15 @@
 
 class Neuro
 {
+private:
+    ThreeDimVector<float> neurons;
+    ThreeDimVector<float> weights;
+    uint16_t layers;
+    QVector<size_t> neuronAmountPerLayer;
+    void generateWeights();
+
 public:
-    Neuro();
+    Neuro(uint16_t l, QVector<size_t> nAPL);
 };
 
 #endif // NEURO_H
