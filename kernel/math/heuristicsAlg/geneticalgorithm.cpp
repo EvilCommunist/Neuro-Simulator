66d30ebfce5edaef7327521d51c36d362eb21c3a
EvilCommunist <AT2415216@yandex.ru>
2025-11-09 15:14:32 +0300

    Starting making GA

--- kernel/math/heuristicsAlg/geneticalgorithm.cpp
+++ kernel/math/heuristicsAlg/geneticalgorithm.cpp
@@ -1,3 +1,7 @@
 #include "geneticalgorithm.h"
 
-GeneticAlgorithm::GeneticAlgorithm() {}
+GeneticAlgorithm::GeneticAlgorithm(size_t popSize = 500, float pMute = 0.3, float pCross = 0.7):
+    populationSize(popSize),
+    pMutation(pMute),
+    pCrossover(pCross)
+{}
--- kernel/math/heuristicsAlg/geneticalgorithm.h
+++ kernel/math/heuristicsAlg/geneticalgorithm.h
@@ -1,10 +1,18 @@
 #ifndef GENETICALGORITHM_H
 #define GENETICALGORITHM_H
 
+#include "individual.h"
+
 class GeneticAlgorithm
 {
+private:
+    size_t populationSize;
+    float pMutation;
+    float pCrossover;
+    QVector<Individual> best, currentGeneration;
+
 public:
-    GeneticAlgorithm();
+    GeneticAlgorithm(size_t popSize = 500, float pMute = 0.3, float pCross = 0.7);
 };
 
 #endif // GENETICALGORITHM_H
